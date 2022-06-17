84c138cc7f984b68faa1f6ed395c1aa4ba4c53d1
Jonas <jonasbhjulstad@gmail.com>
2022-06-17 18:30:10 +0200

    Initial

---
+++ include/CLCPP_PRNG.hpp
@@ -0,0 +1,8 @@
+#ifndef CLCPP_PRNG_FilePaths_hpp
+#define CLCPP_PRNG_FilePaths_hpp
+
+const char* CLCPP_PRNG_INCLUDE_DIR = "/home/deb/Documents/CLCPP_PRNG/include/";
+const char* CLCPP_RANDOMCL_INCLUDE_DIR = "/home/deb/Documents/CLCPP_PRNG/build/_deps/randomcl_repo-src/generators/";
+const char* CLCPP_PRNG_INCLUDE = "-I /home/deb/Documents/CLCPP_PRNG/include/ -I /home/deb/Documents/CLCPP_PRNG/build/_deps/randomcl_repo-src/generators/";
+
+#endif
